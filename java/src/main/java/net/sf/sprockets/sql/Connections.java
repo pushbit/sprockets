cc0b4aec39e68f39e4ae2de2f9e48f332da9ccf9
pushbit <pushbit@gmail.com>
2014-07-12 19:43:37 +0200

    also check that the connection isn't already closed before manipulating it

--- java/src/main/java/net/sf/sprockets/sql/Connections.java
+++ java/src/main/java/net/sf/sprockets/sql/Connections.java
@@ -1,18 +1,18 @@
 /*
- * Copyright 2013 pushbit <pushbit@gmail.com>
- *
+ * Copyright 2013-2014 pushbit <pushbit@gmail.com>
+ * 
  * This file is part of Sprockets.
- *
+ * 
  * Sprockets is free software: you can redistribute it and/or modify it under the terms of the GNU
  * Lesser General Public License as published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
- *
+ * 
  * Sprockets is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with Sprockets.
- * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with Sprockets. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 
 package net.sf.sprockets.sql;
@@ -44,7 +44,7 @@ public class Connections {
 	 *            may be null
 	 */
 	public static void close(Connection con) throws SQLException {
-		if (con != null) {
+		if (con != null && !con.isClosed()) {
 			if (!con.getAutoCommit()) {
 				con.rollback();
 				con.setAutoCommit(true);
